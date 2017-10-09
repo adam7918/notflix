4af3a839f5ecfe59f355ced2533e8420afb1c0db
Adam Summerfield <summerfield.adam@gmail.com>
2017-10-09 16:58:10 +0200

    Updated schemas

--- models/rating.js
+++ models/rating.js
@@ -4,7 +4,7 @@ var Schema       = mongoose.Schema;
 var RatingSchema   = new Schema({
     username : { type : String, required: true },
     imdbtt : { type : Number, required: true },
-    rating: { type : Number, required: true },
+    rating: { type : Number, required: true }
 });
 
 module.exports = mongoose.model('Rating', RatingSchema);
