5185dae6e40eba8e9bd3cb357c09c863582623fe
Adam Summerfield <summerfield.adam@gmail.com>
2017-10-09 17:11:19 +0200

    stuff

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,13 +1,35 @@
 var express = require('express');
-
 var router = express.Router();
+var Movie = require('../models/movie');
 
+// GET ALL MOVIES
 router.get('/', function(req, res) {
-    res.send('GET handler for /movies route.');
+    Movie.find(function(err, movies) {
+        if (err)
+            res.send(err);
+
+        res.json(movies);
+    });
 });
 
+// USED FOR TESTING
 router.post('/', function(req, res) {
-    res.send('POST handler for /movies route.');
+    var movie = new Movie();
+    movie.imdbtt = req.body.imdbtt;
+    movie.title = req.body.title;
+    movie.publicationDate = req.body.publicationDate;
+    movie.length = req.body.length;
+    movie.director = req.body.director;
+    movie.description = req.body.description;
+
+    movie.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'Movie created!' });
+    });
+
 });
+
 
 module.exports = router;
