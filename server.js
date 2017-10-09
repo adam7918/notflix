01fc469fea92dc6ef1ece988d5e8f67db58295aa
Adam Summerfield <summerfield.adam@gmail.com>
2017-10-09 15:46:26 +0200

    init

---
+++ server.js
@@ -0,0 +1,42 @@
+// Call packages needed
+var express    = require('express');
+    users    = require('./routes/userRoutes'),
+    movies    = require('./routes/movieRoutes'),
+    ratings   = require('./routes/ratingRoutes');
+var bodyParser = require('body-parser');
+var app        = express();
+
+// Configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set port
+var port = process.env.PORT || 3000;
+
+// Database setup
+var mongoose   = require('mongoose');
+var mongoDB = 'mongodb://localhost/database';
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+
+// Get the default connection
+var db = mongoose.connection;
+
+// Handle connection events
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("DB connection successful");
+});
+
+// Create our router
+var router = express.Router();
+
+// Routes for API
+app.use('/users',  users);
+app.use('/movies',  movies);
+app.use('/ratings', ratings);
+
+// Start the server
+app.listen(port);
+console.log('Server running on port: ' + port);
