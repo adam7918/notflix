732dad01e744ffd425a49dbe5946928256be9c67
Adam Summerfield <summerfield.adam@gmail.com>
2017-10-12 11:49:36 +0200

    stuff

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,15 +2,32 @@ var express = require('express');
 var router = express.Router();
 var Movie = require('../models/movie');
 
-// GET ALL MOVIES
+// GET - Returns all movies in a JSON array
 router.get('/', function(req, res) {
     Movie.find(function(err, movies) {
+        // Server error
         if (err) res.status(500).json("Server error");
+        // Returns movie array and status 200
         res.status(200).json(movies);
     });
 });
 
-// USED FOR TESTING
+// GET - Returns movie based on ID param given by user
+router.get('/:imdbtt', function(req, res) {
+    Movie.findOne({"imdbtt" : req.params.imdbtt},function(err, movie) {
+        // Server error
+        if (err) res.status(500).json("Server error");
+        // 404 error if movie with imdbtt not found
+        if(movie === null){
+            res.status(404).json("Movie not found");
+        } else {
+            // Returns movie with status 200
+            res.status(200).json(movie);
+        }
+    });
+});
+
+// =========USED FOR TESTING===========DELETE AFTER
 router.post('/', function(req, res) {
     var movie = new Movie();
     movie.imdbtt = req.body.imdbtt;
@@ -27,11 +44,5 @@ router.post('/', function(req, res) {
     });
 });
 
-router.get('/:imdbtt', function(req, res) {
-    Movie.findOne({"imdbtt" : req.params.id},function(err, movies) {
-        if (err) res.status(500).json("Server error");
-        res.status(200).json(movies);
-    });
-});
-
+// Export
 module.exports = router;
