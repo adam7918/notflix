77c60f841bf46bb1e1bc5a608ff27635ebe2c6b7
BuildTools <unconfigured@null.spigotmc.org>
2017-10-14 14:50:16 +0200

    Added test file and added error codes to access token system

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -8,7 +8,7 @@ module.exports = {
             // verifies secret and checks exp
             jwt.verify(token, req.app.get('private-key'), function (err, decoded) {
                 if (err) {
-                    return res.json({success: false, message: 'Failed to authenticate token.'});
+                    return res.status(401).json({success: false, message: 'Failed to authenticate token.'});
                 } else {
                     // if everything is good, save to request for use in other routes
                     req.decoded = decoded;
