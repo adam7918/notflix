a454776ac67c3556e7fac5fb6eb90dafe42e7099
BuildTools <unconfigured@null.spigotmc.org>
2017-10-15 18:36:13 +0200

    Added tests

--- testing.js
+++ testing.js
@@ -1,8 +1,8 @@
 var supertest = require('supertest');
 // Define the url of your HTTP-server
 var server = supertest.agent("http://localhost:3000");
+var token;
 describe("Users", function(){
-    var token;
     it("should create a test user", function(done){
         var user = {firstName:'Evert', lastName:'Duipman', middleName:'Test', username:'Test', password:'Test01!'};
         server
@@ -33,8 +33,35 @@ describe("Users", function(){
     });
 });
 describe("Movies", function(){
-    it("should add a movie", function(done){
+    it("should list all movies", function(done){
         server.get("/movies")
+            .expect("Content-type", /json/)
+            .expect(200, done);
+    });
+    it("should get a movie specified by the url", function(done){
+        server.get("/movies/1")
+            .expect("Content-type", /json/)
+            .expect(200, done);
+    });
+});
+describe("Ratings", function(){
+    it("User should rate movie", function(done){
+        var rate = {rating:'5'};
+        server.put("/users/ratings/1")
+            .set('authorization', token)
+            .send(rate)
+            .expect("Content-type", /json/)
+            .expect(200 || 201, done);
+    });
+    it("User removes his rating", function(done){
+        server.delete("/user/ratings/1")
+            .set('authorization', token)
+            .expect("Content-type", /json/)
+            .expect(204, done);
+    });
+    it("User sees his own rating", function(done){
+        server.delete("/user/ratings/1")
+            .set('authorization', token)
             .expect("Content-type", /json/)
             .expect(200, done);
     });
