673642aaaaf12d4b6230ce224f373fc090e7b706
Adam Summerfield <summerfield.adam@gmail.com>
2017-11-02 18:06:00 +0100

    jkedfgjklfxgjfkl

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,14 +1,34 @@
 $.ajax({
     type: "GET",
     url: "/movies/",
-    success: function(data){
-        $.each(data, function(i, movie){
-            $(".container-fluid").append('<div class="movie-container">\n' +
-                '            <div class="movie-rating">8.1</div>\n' +
-                '            <div class="movie" style="background:url(' + movie.poster + ')">\n' +
-                '            </div>\n' +
-                '            <h2>' + movie.title + '</h2>\n' +
-                '        </div>')
+    success: function (data) {
+        $.each(data, function (i, movie) {
+            $.ajax({
+                type: "GET",
+                url: "/movies/ratings/" + movie.imdbtt,
+                success: function (data) {
+                    var movieRating;
+                    try {
+                        movieRating = data[0].rating;
+                    } catch (e) {
+                        if (e) {
+                            movieRating = "N/A";
+                        }
+                    }
+                    $(".container-fluid").append('<a style="display:block" href="#"><div class="movie-container">\n' +
+                        '            <div class="movie-rating">' + movieRating + '</div>\n' +
+                        '            <div class="movie" style="background:url(' + movie.poster + ')">\n' +
+                        '            </div>\n' +
+                        '            <h2>' + movie.title + '</h2>\n' +
+                        '        </div></a>');
+                },
+                error: [
+                    function () {
+                        alert(movie.title);
+                    }
+                ]
+            });
         });
     }
 });
+
---
+++ public_html/js/userlist.js
@@ -0,0 +1,7 @@
+$.ajax({
+    type: "GET",
+    url: "/movies/",
+    success: function (data) {
+
+    }
+});
